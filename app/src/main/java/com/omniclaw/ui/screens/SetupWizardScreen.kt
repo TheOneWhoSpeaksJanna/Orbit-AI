745baab5b10fb6b12f6d4b0f29e97309cb88f529
Z User <z@container>
2026-07-06 17:50:46 +0000

    fix: add missing 'sp' import for live log font size

--- app/src/main/java/com/omniclaw/ui/screens/SetupWizardScreen.kt
+++ app/src/main/java/com/omniclaw/ui/screens/SetupWizardScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.omniclaw.R
 import com.omniclaw.ui.components.AnimatedGlassCard
